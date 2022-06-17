c390a0bb667f1e69f6b3c9ef0aa795e87813abac
kayki.garcia <kayki.garcia@lighthouseit.com.br>
2022-06-17 16:08:09 -0300

    add: zema

--- src/.env.ts
+++ src/.env.ts
@@ -3,45 +3,39 @@ import {SvgProps} from 'react-native-svg';
 import {Credentials} from './http-auth';
 import {TabsConfig} from './components';
 
-import SplashLogo from './assets/images/logo.svg';
-import UserIcon from './assets/icons/user.svg';
 import CartIcon from './assets/icons/cart.svg';
 import HomeIcon from './assets/icons/home.svg';
 
+import SplashLogo from './assets/images/logo.svg';
 type Env = {
   url: string;
   credentials: Credentials;
   splashLogo: React.FC<SvgProps>;
   showTabs: boolean;
-  tabsConfig: TabsConfig;
+  tabsConfig?: TabsConfig;
 };
 
 const ENV: Env = {
-  url: 'https://dev.davo.com.br/',
+  url: '<url>',
   credentials: {
-    user: '',
-    pass: '',
+    user: '<user>',
+    pass: '<pass>',
   },
   splashLogo: SplashLogo,
-  showTabs: false,
+  showTabs: true,
   tabsConfig: {
     initialRoute: 'Home',
-    labelActiveColor: '#e30613',
-    labelInactiveColor: '#f18288',
+    labelActiveColor: '#004a8f',
+    labelInactiveColor: '#bfd1e3',
     routes: [
       {
-        name: 'Login',
-        url: 'https://dev.davo.com.br/login',
-        icon: UserIcon,
-      },
-      {
         name: 'Home',
-        url: 'https://dev.davo.com.br/',
+        url: '<url>',
         icon: HomeIcon,
       },
       {
         name: 'Carrinho',
-        url: 'https://dev.davo.com.br/checkout-shipping',
+        url: '<url>cart',
         icon: CartIcon,
       },
     ],
