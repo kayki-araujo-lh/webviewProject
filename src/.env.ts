8e09778b90ea3ac6323c0091b24ea16c0e59d7f3
kayki.garcia <kayki.garcia@lighthouseit.com.br>
2022-06-17 15:31:48 -0300

    add: .env

---
+++ src/.env.ts
@@ -0,0 +1,51 @@
+import {SvgProps} from 'react-native-svg';
+
+import {Credentials} from './http-auth';
+import {TabsConfig} from './components';
+
+import SplashLogo from './assets/images/logo.svg';
+import UserIcon from './assets/icons/user.svg';
+import CartIcon from './assets/icons/cart.svg';
+import HomeIcon from './assets/icons/home.svg';
+
+type Env = {
+  url: string;
+  credentials: Credentials;
+  splashLogo: React.FC<SvgProps>;
+  showTabs: boolean;
+  tabsConfig: TabsConfig;
+};
+
+const ENV: Env = {
+  url: 'https://www.webcontinental.com.br/',
+  credentials: {
+    user: '',
+    pass: '',
+  },
+  splashLogo: SplashLogo,
+  showTabs: false,
+  tabsConfig: {
+    initialRoute: 'Home',
+    labelActiveColor: '#0098ff',
+    labelInactiveColor: '#676666',
+    routes: [
+      {
+        name: 'Login',
+        url: 'https://www.webcontinental.com.br/login',
+        icon: UserIcon,
+      },
+      {
+        name: 'Home',
+        url: 'https://www.webcontinental.com.br/',
+        icon: HomeIcon,
+      },
+      {
+        name: 'Carrinho',
+        url: 'https://www.webcontinental.com.br/cart',
+        icon: CartIcon,
+      },
+    ],
+  },
+};
+
+export default ENV;
